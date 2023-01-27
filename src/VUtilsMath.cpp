2d47fc68a59bdbd85886ea1ae6112ef391bc8855
PeriodicSeizures <47781580+PeriodicSeizures@users.noreply.github.com>
2023-01-27 12:51:10 -0500

    improve precision

--- src/VUtilsMath.cpp
+++ src/VUtilsMath.cpp
@@ -147,7 +147,7 @@ namespace VUtils::Math {
         i = 0x5f3759df - (i >> 1);               // what the fuck? 
         y = *(float*)&i;
         y = y * (threehalfs - (x2 * y * y));   // 1st iteration
-        //    y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
+        y = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
 
         return y;
     }
