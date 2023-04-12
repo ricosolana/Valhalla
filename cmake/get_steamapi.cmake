fd885c9e2f0eb883bddb1160948f8e03d99a3371
PeriodicSeizures <47781580+PeriodicSeizures@users.noreply.github.com>
2023-04-12 15:51:42 -0400

    broken

--- cmake/get_steamapi.cmake
+++ cmake/get_steamapi.cmake
@@ -4,10 +4,17 @@
 #set(STEAMAPI_SOURCE_DIR "${STEAMAPI_LOCATION}/sdk/public/steam")
 #set(STEAMAPI_SHARED_DIR "${STEAMAPI_LOCATION}/sdk/redistributable_bin/win64/steam_api64.dll")
 
-set(STEAMAPI_LOCATION "C:/Users/Rico/Documents/Visual Studio 2022/Libraries/steam-sdk")
+set(STEAMAPI_LOCATION "/home/pi/steam-api")
 set(STEAMAPI_BINARY_DIR "${STEAMAPI_LOCATION}/sdk/redistributable_bin/win64/steam_api64.lib")
 set(STEAMAPI_SOURCE_DIR "${STEAMAPI_LOCATION}/sdk/public/steam")
 set(STEAMAPI_SHARED_DIR "${STEAMAPI_LOCATION}/sdk/redistributable_bin/win64/steam_api64.dll")
+
+#else ()
+#	set(STEAMAPI_LOCATION "C:/Users/Rico/Documents/Visual Studio 2022/Libraries/steam-sdk")
+#	set(STEAMAPI_BINARY_DIR "${STEAMAPI_LOCATION}/sdk/redistributable_bin/win64/steam_api64.lib")
+#	set(STEAMAPI_SOURCE_DIR "${STEAMAPI_LOCATION}/sdk/public/steam")
+#	set(STEAMAPI_SHARED_DIR "${STEAMAPI_LOCATION}/sdk/redistributable_bin/win64/steam_api64.dll")
+#endif ()
 
 #if(NOT (EXISTS ${STEAMAPI_ZIP_LOCATION}))
 #  message(STATUS "Downloading Steam SDK to ${STEAMAPI_ZIP_LOCATION}")
