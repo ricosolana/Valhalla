20106b5a4fab2c9a9ad775da3f3d9328d9d8a1af
PeriodicSeizures <47781580+PeriodicSeizures@users.noreply.github.com>
2023-05-14 08:38:42 -0400

    failed cmake attempt

---
+++ cmake/get_luajit.cmake
@@ -0,0 +1,4 @@
+set(LUAJIT_LOCATION "C:/Users/Rico/Documents/Visual Studio 2022/Libraries/luajit_install")
+set(LUAJIT_BINARY_DIR "${LUAJIT_LOCATION}/luajit.lib")
+#set(LUAJIT_SOURCE_DIR "${LUAJIT_LOCATION}/sdk/public/steam")
+set(LUAJIT_SHARED_DIR "${LUAJIT_LOCATION}/lua51.dll")
