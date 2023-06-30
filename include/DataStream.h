d8b3fd9c892e6aefe7763c00b5b9f3d77c0a62ed
PeriodicSeizures <47781580+PeriodicSeizures@users.noreply.github.com>
2023-06-30 11:02:33 -0400

    wip
    tellg and tellp apparently unreliable for size of file
    https://stackoverflow.com/questions/2409504/using-c-filestreams-fstream-how-can-you-determine-the-size-of-a-file

--- include/DataStream.h
+++ include/DataStream.h
@@ -4,70 +4,6 @@
 
 #include "VUtils.h"
 
-class SharedFile {
-private:
-    std::FILE* m_file;
-    bool m_isRead;
-    bool m_isOwned;
-
-public:
-    SharedFile(const char* path, bool read) {
-        this->m_file = std::fopen(path, read ? "rb" : "wb");
-        //if (!this->m_file)
-            //throw std::runtime_error("unable to open file");
-
-        this->m_isRead = read;
-        this->m_isOwned = true;
-    }
-    
-    // how to handle shared cases
-    SharedFile(FILE* file, bool read) {
-        this->m_file = file;
-        //if (!this->m_file)
-            //throw std::runtime_error("unable to open file");
-
-        this->m_isRead = read;
-        this->m_isOwned = false;
-    }
-
-    ~SharedFile() {
-        Close();
-    }
-
-    SharedFile(const SharedFile& other) {
-        this->m_file = other.m_file;
-        this->m_isOwned = false; // important to avoid UB
-        this->m_isRead = other.m_isRead;
-    }
-
-    SharedFile(SharedFile&& other) {
-        *this = std::move(other);
-    }
-
-    void operator=(const SharedFile&) = delete;
-
-    void operator=(SharedFile&& other) {
-        this->m_file = other.m_file;
-        other.m_file = nullptr;
-    }
-
-    bool Close() {
-        if (this->m_file && this->m_isOwned) {
-            bool status = std::fclose(this->m_file) == 0;
-            this->m_file = nullptr;
-            return status;
-        }
-        return false;
-    }
-
-    operator std::FILE* () {
-        if (!this->m_file)
-            throw std::runtime_error("file was unable to be opened");
-
-        return this->m_file;
-    }
-};
-
 // 56 Bytes w/ <vec, view>
 // 32 Bytes w/ <vec&, view>
 class DataStream {
@@ -76,28 +12,41 @@ public:
     std::variant<
         std::pair<std::reference_wrapper<BYTES_t>, size_t>, 
         std::pair<BYTE_SPAN_t, size_t>, 
-        SharedFile
+        std::ifstream,
+        std::ofstream
     > m_data;
 
 protected:
     explicit DataStream(BYTE_SPAN_t buf) : m_data(std::pair(buf, 0)) {}
     explicit DataStream(BYTES_t& buf) : m_data(std::pair(std::ref(buf), 0)) {}
-    explicit DataStream(SharedFile file) : m_data(std::move(file)) {}
+    explicit DataStream(std::ifstream file) : m_data(std::move(file)) {
+        file.exceptions(std::ifstream::failbit);
+        file.unsetf(std::ios::skipws);
+    }
+    explicit DataStream(std::ofstream file) : m_data(std::move(file)) {
+        file.exceptions(std::ofstream::failbit);
+        file.unsetf(std::ios::skipws);
+    }
 
 public:
     //bool owned() const { return std::get_if< !this->m_data.data(); }
 
-    size_t Position() {
+    size_t Position() const {
         return std::visit(VUtils::Traits::overload{
             [](const std::pair<std::reference_wrapper<BYTES_t>, size_t>& pair) -> size_t { return pair.second; },
             [](const std::pair<BYTE_SPAN_t, size_t>& pair) -> size_t { return pair.second; },
-            [](SharedFile& file) -> size_t {
-                auto pos = std::ftell(file);
-                if (pos == -1)
-                    throw std::runtime_error("failed to get file pos");
+            [](const std::ifstream& f) -> size_t {
+                auto&& file = const_cast<std::ifstream&>(f);
 
-                return (size_t) pos;
-            }
+                file.exceptions(std::ifstream::failbit);
+                return file.tellg();
+            },
+            [](const std::ofstream& f) -> size_t {
+                auto&& file = const_cast<std::ofstream&>(f);
+
+                file.exceptions(std::ifstream::failbit);
+                return file.tellp();
+            },
         }, this->m_data);
     }
 
@@ -121,10 +70,14 @@ public:
                 
                 pair.second = pos;
             },
-            [&](SharedFile& file) {
-                if (std::fseek(file, pos, SEEK_SET) != 0)
-                    throw std::runtime_error("failed to fseek to pos");
-            }
+            [&](std::ifstream& file) {
+                file.clear();
+                file.seekg(pos, std::ios::beg);
+            },
+            [&](std::ofstream& file) {
+                file.clear();
+                file.seekp(pos, std::ios::beg);
+            },
         }, this->m_data);
     }
 
@@ -133,28 +86,27 @@ public:
         return std::visit(VUtils::Traits::overload{
             [](const std::pair<std::reference_wrapper<BYTES_t>, size_t> &buf) -> size_t { return buf.first.get().size(); },
             [](const std::pair<BYTE_SPAN_t, size_t> &buf) -> size_t { return buf.first.size(); },
-            [](const SharedFile &f) -> size_t {
-                //fstat()
-                // use ifstream or something
-                // i hate how fstream is 128+ bytes....
-                
-                // This should technically be valid
-                //  and safe because fp is mutable
-                auto&& file = const_cast<SharedFile&>(f);
+            [](const std::ifstream &f) -> size_t {
+                auto&& file = const_cast<std::ifstream&>(f);
 
-                auto prev = std::ftell(file);
-                if (prev == -1)
-                    throw std::runtime_error("failed to get file pos (0)");
-                if (std::fseek(file, 0, SEEK_END) != 0)
-                    throw std::runtime_error("failed to set file pos (1)");
+                file.clear();
 
-                auto s = (size_t) std::ftell(file);
-                if (s == -1)
-                    throw std::runtime_error("failed to reset file pos (2)");
-                if (fseek(file, prev, SEEK_SET) != 0)
-                    throw std::runtime_error("failed to set file pos (3)");
+                file.seekg(0, std::ios::end);
+                auto fileSize = file.tellg();
+                file.seekg(0, std::ios::beg);
 
-                return s;
+                return fileSize;
+            },
+            [](const std::ofstream& f) -> size_t {
+                auto&& file = const_cast<std::ofstream&>(f);
+
+                file.clear();
+
+                file.seekp(0, std::ios::end);
+                auto fileSize = file.tellp();
+                file.seekp(0, std::ios::beg);
+
+                return fileSize;
             },
         }, this->m_data);
     }
